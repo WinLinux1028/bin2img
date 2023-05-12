f2822d2aaa8fa3a1565bb85a769bf080e096a396
SovietReimu1228 <reimussr@protonmail.ch>
2023-05-12 18:16:06 +0900

    update

--- wasm_export/src/lib.rs
+++ wasm_export/src/lib.rs
@@ -3,12 +3,14 @@ use bin_img_conv::{Bin, BitDepth, ColorType, Img};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn bin_to_img(mut input: &[u8], bit_depth: u8, color_type: u8) -> Vec<u8> {
-    let mut input = Bin::new(&mut input).unwrap();
-    input.1 = BitDepth::from_u8(bit_depth).unwrap();
-    input.2 = ColorType::from_u8(color_type).unwrap();
+pub fn bin_to_img(input: Vec<u8>, bit_depth: u8, color_type: u8) -> Vec<u8> {
+    let mut input_ = Bin::new(&mut input.as_slice()).unwrap();
+    drop(input);
 
-    let output = Img::try_from(input).unwrap();
+    input_.1 = BitDepth::from_u8(bit_depth).unwrap();
+    input_.2 = ColorType::from_u8(color_type).unwrap();
+
+    let output = Img::try_from(input_).unwrap();
 
     output.into()
 }
