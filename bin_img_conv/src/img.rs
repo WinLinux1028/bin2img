8680e3130fc9e689d184bfaecb0ffae85c17827a
SovietReimu1228 <reimussr@protonmail.ch>
2023-05-14 12:04:53 +0900

    update

--- bin_img_conv/src/img.rs
+++ bin_img_conv/src/img.rs
@@ -1,7 +1,7 @@
 use crate::Bin;
 
 use image::GenericImageView;
-use std::{io::Cursor, ops::Deref};
+use std::ops::Deref;
 
 #[derive(Clone)]
 pub struct Img(Vec<u8>);
@@ -43,47 +43,52 @@ impl TryFrom<Img> for Bin {
             (16, 6) => img.into_rgba16().into(),
             _ => return Err("Unsupported color format.".into()),
         };
-        let mut cursor = Cursor::new(&mut input);
-        img.write_to(&mut cursor, image::ImageOutputFormat::Png)?;
-
-        input.shrink_to_fit();
-        drop(img);
 
         // png内のデータを読み込む
-        let decoder = png::Decoder::new(input.as_slice());
-        let mut reader = decoder.read_info()?;
-        let mut input_ = vec![0; reader.output_buffer_size()];
-        reader.next_frame(&mut input_)?;
+        let color_type = img.color();
+        let bytes_per_pixel = usize::from(color_type.bytes_per_pixel());
+        let colors_per_pixel = usize::from(color_type.channel_count());
+        input = img.into_bytes();
 
-        let bytes_per_pixel = reader.info().bytes_per_pixel();
-        drop(reader);
-        drop(input);
+        // バイトエンディアンを変換する
+        if bytes_per_pixel / colors_per_pixel == 2 {
+            let mut i = 0;
+            while i < input.len() {
+                unsafe {
+                    let word: &u16 = std::mem::transmute(&input[i]);
+                    let word = word.to_be_bytes();
+                    *input.get_unchecked_mut(i) = word[0];
+                    *input.get_unchecked_mut(i + 1) = word[1];
+                }
+                i += 2;
+            }
+        }
 
         // データの長さをチェックする
-        if input_.len() <= bytes_per_pixel + 16 {
+        if input.len() <= bytes_per_pixel + 16 {
             return Err("Too short data.".into());
         }
 
         // 管理データを消す
         for _ in 0..bytes_per_pixel {
-            input_.remove(0);
+            input.remove(0);
         }
 
         // paddingの長さを読み込む
-        let padding = u128::from_be_bytes((&input_[0..16]).try_into()?);
+        let padding = u128::from_be_bytes((&input[0..16]).try_into()?);
         let padding = usize::try_from(padding)?;
 
         // paddingの長さの情報を消す
         for _ in 0..16 {
-            input_.remove(0);
+            input.remove(0);
         }
         // paddingを消す
         for _ in 0..padding {
-            input_.pop();
+            input.pop();
         }
 
-        input_.shrink_to_fit();
-        let result = Bin::new_raw(input_);
+        input.shrink_to_fit();
+        let result = Bin::new_raw(input);
         Ok(result)
     }
 }
